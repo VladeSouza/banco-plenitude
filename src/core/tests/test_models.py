8b812e565ba2c45ffa88912b1c86bb52fbc7a016
vladetec <vladetec@gmail.com>
2022-08-30 00:00:01 -0300

    Adding tdd superuser and model

--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -19,3 +19,13 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
